e353d1e0be284f8d4374c9d17c6eac32c94e82f6
chaeyeon <kidsland09@naver.com>
2023-03-17 13:51:37 +0900

    initial setting

---
+++ linklab/handout/part1/memtrace.c
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+//
+// memtrace
+//
+// trace calls to the dynamic memory manager
+//
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <memlog.h>
+#include <memlist.h>
+
+//
+// function pointers to stdlib's memory management functions
+//
+static void *(*mallocp)(size_t size) = NULL;
+static void (*freep)(void *ptr) = NULL;
+static void *(*callocp)(size_t nmemb, size_t size);
+static void *(*reallocp)(void *ptr, size_t size);
+
+//
+// statistics & other global variables
+//
+static unsigned long n_malloc  = 0;
+static unsigned long n_calloc  = 0;
+static unsigned long n_realloc = 0;
+static unsigned long n_allocb  = 0;
+static unsigned long n_freeb   = 0;
+static item *list = NULL;
+
+//
+// init - this function is called once when the shared library is loaded
+//
+__attribute__((constructor))
+void init(void)
+{
+  char *error;
+
+  LOG_START();
+
+  // initialize a new list to keep track of all memory (de-)allocations
+  // (not needed for part 1)
+  list = new_list();
+
+  // ...
+}
+
+//
+// fini - this function is called once when the shared library is unloaded
+//
+__attribute__((destructor))
+void fini(void)
+{
+  // ...
+
+  LOG_STATISTICS(0L, 0L, 0L);
+
+  LOG_STOP();
+
+  // free list (not needed for part 1)
+  free_list(list);
+}
+
+// ...
