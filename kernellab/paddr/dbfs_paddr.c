7f6fbd95998e5ca0e629b0c7768bfd5983412549
chaeyeon <kidsland09@naver.com>
2023-05-22 13:05:08 +0900

    comments added

--- kernellab/paddr/dbfs_paddr.c
+++ kernellab/paddr/dbfs_paddr.c
@@ -10,75 +10,87 @@ MODULE_LICENSE("GPL");
 static struct dentry *dir, *output;
 static struct task_struct *task;
 
+// struct packet : to store data from the argument 'user_buffer' in the read_output function
 struct packet {
         pid_t pid;
         unsigned long vaddr;
         unsigned long paddr;
 };
 
-
+// read_output : to read virtual address from user_buffer and convert it to physical address doing page walk
 static ssize_t read_output(struct file *fp,
                         char __user *user_buffer,
                         size_t length,
                         loff_t *position)
 {
-        // Implement read file operation
         struct packet pack;
         unsigned long ppn, ppo;
 
+        // to copy the data from user_buffer to 'pack'
         copy_from_user(&pack, user_buffer, length);
+
+        // to find the task_struct corresponding to the given pid
         task = pid_task(find_vpid(pack.pid), PIDTYPE_PID);
         if (!task){
                 printk("Process with the typed pid does not exist\n");
                 return -1;
         }
 
+        // to get pgd from mm_struct and the virtual address - page walk step 1
         pgd_t *pgd = pgd_offset(task->mm, pack.vaddr);
         if (pgd_none(*pgd) || pgd_bad(*pgd)){
                 printk("Error related to pgd\n");
                 return -1;
         }
 
+        // to get p4d from pgd and the virtual address - page walk step 2
         p4d_t *p4d = p4d_offset(pgd, pack.vaddr);
         if (p4d_none(*p4d) || p4d_bad(*p4d)){
                 printk("Error related to p4d\n");
                 return -1;
         }
 
+        // to get pud from p4d and the virtual address - page walk step 3
         pud_t *pud = pud_offset(p4d, pack.vaddr);
         if (pud_none(*pud) || pud_bad(*pud)){
                 printk("Error related to pud\n");
                 return -1;
         }
 
+        // to get pmd from pud and the virtual address - page walk step 4
         pmd_t *pmd = pmd_offset(pud, pack.vaddr);
         if (pmd_none(*pmd) || pmd_bad(*pmd)){
                 printk("Error related to pmd\n");
                 return -1;
         }
         
+        // to get pte from pmd and the virtual address - page walk step 5
         pte_t *pte = pte_offset_map(pmd, pack.vaddr);
         if (pte_none(*pte)){
                 printk("Error related to pte\n");
                 return -1;
         }
-        
+
+        // to calculate ppn and ppo and concatenate them to get the physical address
         ppn = pte_pfn(*pte) << PAGE_SHIFT;
         ppo = pack.vaddr & (~PAGE_MASK);
         pack.paddr = ppn | ppo;
 
+        // to copy the data from 'pack to user_buffer
         copy_to_user(user_buffer, &pack, length);
-        return 0;
+        
+        return length;
 }
 
+// stuct file_operations : to map file operations with my function
 static const struct file_operations dbfs_fops = {
-        // Mapping file operations with your functions
         .read = read_output
 };
 
+// dbfs_module_init : It is running when the module is loaded to the kernel
 static int __init dbfs_module_init(void)
 {
-        // Implement init module
+        // to create the directory 'paddr' in the debugfs system
         dir = debugfs_create_dir("paddr", NULL);
 
         if (!dir) {
@@ -86,20 +98,23 @@ static int __init dbfs_module_init(void)
                 return -1;
         }
 
-        // Fill in the arguments below
+        // to create the file 'output' in the 'paddr' directory
         output = debugfs_create_file("output", 0777, dir, NULL, &dbfs_fops);
 
+        // to print the log that it is initialized successfully
 	printk("dbfs_paddr module initialize done\n");
 
         return 0;
 }
 
+// dbfs_module_exit: It is running when the module is unloaded from the kernel
 static void __exit dbfs_module_exit(void)
 {
         // recursively removes a 'dir' directory
         debugfs_remove_recursive(dir);
         dir = NULL;
 
+        // to print the log when the module is unloaded succesfully
 	printk("dbfs_paddr module exit\n");
 }
 
--- kernellab/ptree/dbfs_ptree.c
+++ kernellab/ptree/dbfs_ptree.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
@@ -9,6 +10,7 @@ static struct dentry *dir, *inputdir, *ptreedir;
 static struct task_struct *curr;
 static struct debugfs_blob_wrapper *blob;
 
+// write_pid_to_input : trace process from the process corresponding to the given pid to the init process
 static ssize_t write_pid_to_input(struct file *fp, 
                                 const char __user *user_buffer, 
                                 size_t length, 
@@ -17,18 +19,20 @@ static ssize_t write_pid_to_input(struct file *fp,
         pid_t input_pid;
         char tp[5000];
 
+        // to get the pid 'input_pid' from the input
         sscanf(user_buffer, "%u", &input_pid);
 
-        // Find task_struct using input_pid. Hint: pid_task
+        // to find task_struct 'curr' using input_pid
         curr = pid_task(find_vpid(input_pid), PIDTYPE_PID);
         if (!curr){
                 printk("Process with the typed pid does not exist.");
                 return -1;
         }
-        // Tracing process tree from input_pid to init(1) process
-        // Make Output Format string: process_command (process_id)
+
+        // to empty the blob->data before tracing the process tree
         ((char *)(blob->data))[0] = NULL;
 
+        // Tracing process tree from input_pid to init(1) process and store "process_command (pid)" in blob->data
         while (curr){
                 sprintf(tp, "%s (%d)\n%s", curr->comm, curr->pid, (char *)(blob->data));
                 sprintf(blob->data, "%s", tp);
@@ -39,47 +43,54 @@ static ssize_t write_pid_to_input(struct file *fp,
         return length;
 }
 
+// stuct file_operations : to map file operations with my function
 static const struct file_operations dbfs_fops = {
         .write = write_pid_to_input,
 };
 
+// dbfs_module_init : It is running when the module is loaded to the kernel
 static int __init dbfs_module_init(void)
 {
-        // Implement init module code
+        // to create the directory 'ptree' in the debug file system
         dir = debugfs_create_dir("ptree", NULL);
-        
         if (!dir) {
                 printk("Cannot create ptree dir\n");
                 return -1;
         }
 
+        // to allocate and initialize 'struct debugfs_blob_wrapper *'
         blob = (struct debugfs_blob_wrapper *)kmalloc(sizeof(struct debugfs_blob_wrapper), GFP_KERNEL);
         blob -> data = (void *)kmalloc(5000, GFP_KERNEL);
         blob -> size = 5000;
-        tot_len = 0;
  
+        // create files in the debug file system
         inputdir = debugfs_create_file("input", 0777, dir, NULL, &dbfs_fops);
-        ptreedir = debugfs_create_blob("ptree", 0777, dir, blob); // Find suitable debugfs API	
+        ptreedir = debugfs_create_blob("ptree", 0777, dir, blob);
 
+        //to print the log message when the module is loaded successfully
 	printk("dbfs_ptree module initialize done\n");
         return 0;
 }
 
+// dbfs_module_exit: It is running when the module is unloaded from the kernel
 static void __exit dbfs_module_exit(void)
 {
-        // Implement exit module code
+        // recursively removes a 'dir' directory
         debugfs_remove_recursive(dir);
         
+        // to deallocate the 'blob -> data'
         if (blob && blob->data){
                 kfree(blob->data);
                 blob->data = NULL;
         }
 
+        // to deallocate the 'blob'
         if (blob){
                 kfree(blob);
                 blob = NULL;
         }
 	
+        // to print the log when the module is unloaded succesfully
         printk("dbfs_ptree module exit\n");
 }
 
